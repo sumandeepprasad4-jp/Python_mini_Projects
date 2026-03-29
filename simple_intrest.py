bb6b99379a60e910a1b8124310c19d260ee0cc27
Sumandeep prasad <sumandeepprasad4@gmail.com>
2026-03-30 01:10:09 +0530

    Delete simple_intrest.py

--- simple_intrest.py
+++
@@ -1,5 +0,0 @@
-principal=float(input("enter the principal:"))
-rate=float(input("enter the rate:"))
-time=float(input("enter the rate"))
-simple_intrest=(principal*rate*time)/100
-print("simple intreat is ",simple_intrest)
