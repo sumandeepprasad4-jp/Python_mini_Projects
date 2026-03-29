e8e70f74ac371faa0e2fa2bfc091e59e790ac94e
Sumandeep prasad <sumandeepprasad4@gmail.com>
2026-03-30 01:09:30 +0530

    Delete compound_intrest.py

--- compound_intrest.py
+++
@@ -1,6 +0,0 @@
-principal=float(input("enter the principal"))
-rate=float(input("enter the rate"))
-time=float(input("enter the time"))
-amount=principal*(1+rate/100)**time
-ci=amount-principal
-print("copound intrest is",ci)
